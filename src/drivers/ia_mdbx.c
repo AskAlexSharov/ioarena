187695213638c8c9ebae0b0a106c3fdac17825bc
alex.sharov <AskAlexSharov@gmail.com>
2020-11-27 11:20:10 +0700

    aa

--- src/drivers/ia_mdbx.c
+++ src/drivers/ia_mdbx.c
@@ -279,8 +279,9 @@ static int ia_mdbx_next(iacontext *ctx, iabenchmark step, iakv *kv) {
     k.iov_len = kv->ksize;
     v.iov_base = kv->v;
     v.iov_len = kv->vsize;
-    rc = mdbx_cursor_renew(ctx->txn, ctx->cursor);
-    rc = mdbx_cursor_put(self->dbi, &k, &v, MDBX_NOOVERWRITE);
+    rc = mdbx_put(ctx->txn, self->dbi, &k, &v, 0);
+//    rc = mdbx_cursor_renew(ctx->txn, ctx->cursor);
+//    rc = mdbx_cursor_put(self->dbi, &k, &v, MDBX_NOOVERWRITE);
     if (rc != MDBX_SUCCESS)
       goto bailout;
     break;
